14d554838504c8ff1f0c6fe523e2124cb1697d61
kennielima <kennylima970@gmail.com>
2024-01-24 09:03:40 +0100

    CRUD camp

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const methodOverride = require('method-override');
+const ExpressError = require('./utils/ExpressError');
+
+const reviews = require('./routes/reviews');
+const campgrounds = require('./routes/campgrounds');
+
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'asecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something went Wrong';
+    res.status(statusCode).render('error', { err });
+    // res.send('something went wrong')
+})
+
+app.listen(3000, () => {
+    console.log('serving on port 3000')
+})
